ab7e93302f483bfc203dbb744016638d94eadd44
Theo Upton <Theo.Upton+Git@gmail.com>
2026-05-05 17:55:26 +0100

    chore(test): add vitest.config.js and editor alias config

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    test: {
+        alias: {
+            '@public': resolve(__dirname, 'public'),
+            '@src': resolve(__dirname, 'src'),
+        }
+    }
+});
