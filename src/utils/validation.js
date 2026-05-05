a0a1d1c0ee01b43d50c1e21e34b889a52dd3a2c1
Theo Upton <Theo.Upton+Git@gmail.com>
2026-05-05 18:18:13 +0100

    fix(util): enforce email max length 255

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,4 @@
 /** @param {string} email  */
 export const isValidEmail = (email) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  return (email.length <= 255) && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
