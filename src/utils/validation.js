eec4647623203db9997b98fd0a182e19f7c13117
Theo Upton <Theo.Upton+Git@gmail.com>
2026-05-05 19:06:07 +0100

    feat(utils): add tests for password validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,3 +2,14 @@
 export const isValidEmail = (email) => {
   return (email.length <= 255) && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
+/** @param {string} password  */
+export const isValidPassword = (password) => {
+  if (typeof password !== 'string') return false;
+  if(password.length > 127) return false;
+  if(/\s/.test(password)) return false; //no spaces
+  if(!/[a-z]/.test(password)) return false; //no lowercase chars
+  if(!/[A-Z]/.test(password)) return false; //no uppercase chars
+  if(!/\d/.test(password)) return false; //no digits
+  if(!/[^A-Za-z0-9]/.test(password)) return false; //no special characters
+  return true;
+};
